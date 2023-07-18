009e7b067ba3a1673f9cea5d964a49b65462cd89
Vinicius <viniciuszaneti.vz@gmail.com>
2023-07-18 20:28:27 -0300

    atualizacao exercicios

--- exercicio07.js
+++ exercicio07.js
@@ -1,9 +1,19 @@
 //7. Calcular a soma dos números de 1 a 100 utilizando a estrutura while.
 
-let numeros = 0;
+let contador = 0;
+let soma = 0;
 
-while (numeros < 100) {
-    numeros = numeros +1
+// while (numeros < 100) {
+//     numeros = numeros +1
 
-    console.log(numeros)
+//     console.log(numeros)
+// }
+
+while(contador <= 100){
+    
+    soma = soma + contador
+    console.log(soma)
+    contador++
+
+
 }
